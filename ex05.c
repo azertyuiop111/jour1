cebccf6bae33ea2e2622e7bc7c4cb7dfbb04d8fd
daoud <dada.lors@bk.ru>
2019-08-07 14:04:05 +0200

    voilai

---
+++ ex05.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+int main (void)
+{
+    int i=1;
+    while(i <=5)
+        
+}
+{
+    
+    printf("%d\n", i);
+    i++;
+}
+{
+    printf("On est au millieu\n");
+}
+{
+    int b=6;
+    while(b <=10)
+        
+}
+{
+    printf("%d\n", b);
+    i++;
+    
+}
+{
+    return 0;
+}
